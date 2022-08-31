cc682ca21958ec3005e88ff78690660058bbb869
Kirill Ivanov <cantti@outlook.com>
2022-09-01 01:03:11 +0300

    Some basic UI

--- src/App.js
+++ src/App.js
@@ -5,10 +5,64 @@ function App() {
     var audio = new Audio("atom://" + result);
     audio.play();
   }
-
   return (
-    <div className="App">
-      <button onClick={handleOpenFile}>Open</button>
+    <div className="window">
+      <div className="window-content">
+        <div className="pane-group">
+          <div className="pane">
+            <ul className="list-group">
+              <li className="list-group-header">Go up</li>
+              <li className="list-group-item">
+                <div className="media-body">
+                  <strong>List item title</strong>
+                  <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+              </li>
+              <li className="list-group-item">
+                <div className="media-body">
+                  <strong>List item title</strong>
+                  <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="pane">
+            <button className="btn btn-primary" onClick={handleOpenFile}>
+              Open audio
+            </button>
+          </div>
+        </div>
+      </div>
+      <footer className="toolbar toolbar-footer">
+        <div className="toolbar-actions">
+          <div className="btn-group">
+            <button className="btn btn-default">
+              <span className="icon icon-home"></span>
+            </button>
+            <button className="btn btn-default">
+              <span className="icon icon-folder"></span>
+            </button>
+            <button className="btn btn-default active">
+              <span className="icon icon-cloud"></span>
+            </button>
+            <button className="btn btn-default">
+              <span className="icon icon-popup"></span>
+            </button>
+            <button className="btn btn-default">
+              <span className="icon icon-shuffle"></span>
+            </button>
+          </div>
+
+          <button className="btn btn-default">
+            <span className="icon icon-home icon-text"></span>
+            Filters
+          </button>
+
+          <button className="btn btn-default btn-dropdown pull-right">
+            <span className="icon icon-megaphone"></span>
+          </button>
+        </div>
+      </footer>
     </div>
   );
 }
