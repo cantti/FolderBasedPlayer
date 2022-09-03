11ca453c736224f67a9ea809e1ff8110b7f5e1fa
Kirill Ivanov <cantti@outlook.com>
2022-09-03 23:21:14 +0300

    Var fix

--- public/handlers/readMetadata.js
+++ public/handlers/readMetadata.js
@@ -2,10 +2,10 @@ const { parseFile } = require('music-metadata');
 
 module.exports = async function (event, path) {
     const metadata = await parseFile(path);
-    let pictureBase64 = '';
+    let pictureLink = '';
     if (metadata.common.picture[0]) {
         const picture = metadata.common.picture[0];
-        pictureBase64 = `data:${picture.format};base64,${picture.data.toString('base64')}`;
+        pictureLink = `data:${picture.format};base64,${picture.data.toString('base64')}`;
     }
-    return { ...metadata, picture: pictureBase64 };
+    return { ...metadata, picture: pictureLink };
 };
